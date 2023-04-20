0132407de82c41d4df7bc38660d94735cfd5305e
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-04-20 23:15:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.regex.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "ggg240.7.97.185kkkkkk";
+        String f = "Это IP адрес 206.157.141.44 kkkkkk";
+        System.out.println(True_IP(s));
+        System.out.println(True_IP(f));
+    }
+
+    static String True_IP(String IP){
+        Pattern p = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)");
+        Matcher m = p.matcher(IP);
+        if (m.find()){
+            return IP.substring(m.start(), m.end());
+        }
+        return "Have not IP in this string";
+    }
+}
